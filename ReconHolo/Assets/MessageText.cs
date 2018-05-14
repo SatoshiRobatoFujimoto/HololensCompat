e94420ded05ca238d13e86dfa47f4d8071c1ca5a
oriamd <ori.amuday@gmail.com>
2018-05-14 20:56:26 +0300

    Initial commit

---
+++ ReconHolo/Assets/MessageText.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class MessageText : MonoBehaviour {
+
+    public Transform cameraTf;
+    public Transform messageTextTf;
+    public TextMesh messageTextTm;
+    public TcpClientHandler tcpClientHandler;
+    public ThreadCompat threadComp;
+    public int count = 0;
+
+    private const String HOST = "192.168.43.228";
+    private const String PORT = "8081";
+    private bool msgWasInit;
+    // Use this for initialization
+    void Start () {
+        // Positioning text
+        // V degrees in the vertical plane and
+        float V = 0;// + deflection
+        //H degrees in the horizontal plane
+        float H = 90;// + deflection
+        float distance = 2; //distance from the camera
+
+        Vector3 vTarget = Quaternion.Euler(V, H, 0) * -Vector3.forward;
+        messageTextTf.position = cameraTf.position - vTarget * distance;
+        threadComp.start(new ThreadCompat.RunFunction(threadFunc));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //msgWasInit = tcpClientHandler.Write("Hello from hololens"+ count);
+        //count++;
+        //messageTextTm.text = tcpClientHandler.Read();
+    }
+   
+    public void threadFunc()
+    {
+        String msg;
+        tcpClientHandler.Connect(HOST, PORT);
+        msgWasInit = tcpClientHandler.Write("mark_target");
+        while (msgWasInit)
+        {
+            try
+            {
+                msg = tcpClientHandler.Read();
+            }
+            catch(Exception e)
+            {
+                return;
+            }
+            if(msg != null)
+            {
+                Debug.Log(msg);
+            }
+        }
+        
+    }
+}
---
+++ ReconHolo/Assets/camera.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class camera : MonoBehaviour {
+
+    public Transform cameraTf;
+    public Transform targetTf;
+    
+    //Demo target location
+    const float lat = 32.0876979f;
+    const float logi = 34.7839517f;
+    const float altitude = 21;
+
+    /// <summary>
+    ///     Calculats target location relative to cameraPosition
+    /// </summary>
+    /// <param name="cameraPosition"></param>
+    /// <param name="distance">Distance from camera</param>
+    /// <param name="v">V degrees in the vertical plane</param>
+    /// <param name="h">H degrees in the horizontal plane</param>
+    public Vector3 calculateTargetLocation(Vector3 cameraPosition, float distance, float v, float h)
+    {
+        Vector3 vTarget = Quaternion.Euler(v, h, 0) * -Vector3.forward;
+        return cameraPosition - vTarget * distance;
+    }
+
+    // Use this for initialization
+    void Start () {
+        //V degrees in the vertical plane and 
+        float V = 45;// + deflection
+        //H degrees in the horizontal plane
+        float H = 45;// + deflection
+        //distance from the camera
+        float distance = 2; 
+        
+        targetTf.position = calculateTargetLocation(cameraTf.position, distance, V, H);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
